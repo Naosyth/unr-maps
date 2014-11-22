69d2eec9069e7c75f8e1ac4769dfc8432847cd97
Battlingtoads <danielgoodnow@gmail.com>
2014-11-21 16:15:16 -0800

    basic nav drawer working. placeholder icons used. Change theme

--- app/src/main/java/com/cs420/unrmaps/MainMap.java
+++ app/src/main/java/com/cs420/unrmaps/MainMap.java
@@ -2,9 +2,15 @@ package com.cs420.unrmaps;
 
 
 import android.app.Activity;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.support.v4.app.ActionBarDrawerToggle;
+import android.support.v4.widget.DrawerLayout;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -13,10 +19,20 @@ import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 
+import java.util.List;
+
 
 public class MainMap extends Activity implements GoogleMap.OnMapClickListener{
     static final LatLng Point = new LatLng(39.544697, -119.816931);
     private GoogleMap googleMap;
+
+    //Navigation Drawer
+    private String[] mNavOptions;
+    private DrawerLayout mDrawerLayout;
+    private ListView mDrawerList;
+    private ActionBarDrawerToggle mDrawerToggle;
+    private CharSequence mDrawerTitle;
+    private CharSequence mTitle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +51,54 @@ public class MainMap extends Activity implements GoogleMap.OnMapClickListener{
         catch(Exception e) {
             e.printStackTrace();
         }
+
+        mNavOptions = getResources().getStringArray(R.array.nav_options);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerList = (ListView) findViewById(R.id.left_drawer);
+
+        mDrawerList.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, mNavOptions));
+        //mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
+
+        mTitle = mDrawerTitle = getTitle();
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.drawable.ic_launcher, R.string.drawer_open, R.string.drawer_close){
+            public void onDrawerClosed(View view){
+                super.onDrawerClosed(view);
+                getActionBar().setTitle(mTitle);
+                invalidateOptionsMenu();
+            }
+
+            public void onDrawerOpened(View drawerView)
+            {
+                super.onDrawerOpened(drawerView);
+                getActionBar().setTitle(mDrawerTitle);
+                invalidateOptionsMenu();
+            }
+        };
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+        getActionBar().setHomeButtonEnabled(true);
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState){
+        super.onPostCreate(savedInstanceState);
+
+        mDrawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig){
+        super.onConfigurationChanged(newConfig);
+        mDrawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu)
+    {
+        boolean drawerOpen = mDrawerLayout.isDrawerOpen(mDrawerList);
+        //hide stuff if necessary
+        return super.onPrepareOptionsMenu(menu);
     }
 
 
@@ -50,6 +114,10 @@ public class MainMap extends Activity implements GoogleMap.OnMapClickListener{
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
+        if(mDrawerToggle.onOptionsItemSelected(item))
+        {
+            return true;
+        }
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
